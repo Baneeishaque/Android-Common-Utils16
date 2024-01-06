9226da6298950f6d5cefeff5b8ee80a0c797c644
Banee Ishaque K <baneeishaque@gmail.com>
2024-01-07 02:55:40 +0530

    IntentExtendedDataItemNames

---
+++ common16/src/main/java/ndk/utils_android16/constants/IntentExtendedDataItemNames.java
@@ -0,0 +1,11 @@
+package ndk.utils_android16.constants;
+
+public class IntentExtendedDataItemNames {
+
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_SELECT_USER_URL = "selectUserUrl";
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_APPLICATION_NAME = "applicationName";
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_NEXT_ACTIVITY_CLASS = "nextActivityClass";
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_TEST_USERNAME = "testUsername";
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_TEST_PASSWORD = "testPassword";
+    public static final String INTENT_EXTENDED_DATA_ITEM_NAME_SHARED_PREFERENCES_KEY_USER_ID = "sharedPreferencesKeyUserId";
+}
